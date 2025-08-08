f1f011c22fbaf11054ff18dd95100f7d5f5f5640
Arnab Sarkar <arnabsarkar236@gmail.com>
2025-08-09 00:42:59 +0530

    Done new commit-routes

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@
 import dotenv from "dotenv"
 
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+// import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
-// connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//       console.log(` Server is running at port : ${process.env.PORT}`)  
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGODB connection failed!  : ",  err);
-// })
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` Server is running at port : ${process.env.PORT}`)  
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed!  : ",  err);
+})
 
 
 
